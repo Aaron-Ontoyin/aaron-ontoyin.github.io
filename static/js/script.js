0c417648ad3c17867934a70efe8a3d7441a68d02
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-09-10 01:07:55 +0000

    enhancement

--- static/js/script.js
+++ static/js/script.js
@@ -1,38 +1,30 @@
 // Smooth scrolling for navigation links
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-        
+function scrollToTarget(targetId) {
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
         window.scrollTo({
             top: targetElement.offsetTop,
             behavior: 'smooth'
         });
-    });
-});
-
-
-// Back to Top button
-const backToTopButton = document.querySelector('.back-to-top');
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 200) {
-        backToTopButton.style.display = 'block';
-    } else {
-        backToTopButton.style.display = 'none';
     }
+}
+
+document.getElementById("cta-button").addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    scrollToTarget(targetId);
 });
 
-backToTopButton.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        scrollToTarget(targetId);
     });
 });
 
-
+// nav toggle
 const menuToggle = document.getElementById('menu-toggle');
 const navbarMenu = document.querySelector('.navbar-menu');
 
@@ -40,10 +32,30 @@ menuToggle.addEventListener('click', function() {
     navbarMenu.classList.toggle('active');
 });
 
-// Close the menu when a link is clicked (optional)
 const navbarLinks = document.querySelectorAll('.navbar-menu a');
 navbarLinks.forEach(function(link) {
     link.addEventListener('click', function() {
         navbarMenu.classList.remove('active');
     });
 });
+
+
+// // Back to Top button
+const backButton = document.getElementById('back-to-top-button');
+
+window.addEventListener('scroll', function () {
+
+    if (document.documentElement.scrollTop > 100) {
+        backButton.style.display = 'block';
+    } else {
+        backButton.style.display = 'none';
+    }
+});
+
+backButton.addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
