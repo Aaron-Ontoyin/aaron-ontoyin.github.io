da28004647803cdc1023f9b71d702108f3eda16c
Aaron Ontoyin Yin <aarononto909@gmail.com>
2024-08-28 14:59:33 +0000

    change gallery

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,7 @@
 const testimonialDiv = $('.testimonial .p-2');
 const testimonialImg = $('.testimonial img');
 
-const galleryContainer = $('.gallery-container-inner');
-const numOfImages = 18;
+const numOfImages = 11;
 
 
 $(document).ready(function () {
@@ -16,20 +15,17 @@ $(document).ready(function () {
         $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
     });
 
-    for (let i = 1; i < numOfImages + 1; i++) {
-        var img = $('<img>');
-        img.attr('src', `static/images/gallery/${i}.webp`);
-        img.addClass('gallery-item', 'img-fluid');
-        img.attr('data-aos', 'fade-left');
-        img.attr('width', 'auto');
-        img.attr('height', 'auto');
-        galleryContainer.append(img);
+    for (let i = 1; i <= numOfImages; i++) {
+        let span = $('<span>').attr('style', `--i:${i}`);
+        let img = $('<img>').attr('src', `static/images/gallery/${i}.webp`).attr('alt', `Image ${i}`);
+        span.append(img);
+        $('.gallery').append(span);
     }
 
     if (window.matchMedia('(max-width: 767px)').matches) {
         $('.testimonial').each(function () {
-            var testimonialDiv = $(this).find('.p-2');
-            var testimonialImg = $(this).find('img');
+            let testimonialDiv = $(this).find('.p-2');
+            let testimonialImg = $(this).find('img');
 
             testimonialImg.clone().insertBefore(testimonialDiv);
             testimonialImg.remove();
@@ -37,31 +33,4 @@ $(document).ready(function () {
     }
 
     $('#copyright-year').html(new Date().getFullYear());
-
-    // var Scrollbar = window.Scrollbar;
-    // var bodyScroll = Scrollbar.init(document.querySelector('body'), {
-    //     damping: 0.05,
-    //     speed: 500,
-    //     thumbMinSize: 5,
-    //     renderByPixels: true,
-    //     alwaysShowTracks: false,
-    //     continuousScrolling: true
-    // });
-
-    // var projectsScroll = Scrollbar.init(document.querySelector('#projects-box'), {
-    //     damping: 0.1,
-    //     thumbMinSize: 5,
-    //     renderByPixels: true,
-    //     alwaysShowTracks: false,
-    //     // continuousScrolling: true
-    // });
-
-    // var testimonialsScroll = Scrollbar.init(document.querySelector('.testimonials-container-inner'), {
-    //     damping: 0.1,
-    //     thumbMinSize: 5,
-    //     renderByPixels: true,
-    //     alwaysShowTracks: false,
-    //     continuousScrolling: true
-    // });
-
 });
